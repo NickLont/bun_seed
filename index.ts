657d5e32d60d67183301e3116fad7dc6d2767dea
Nick Lontorfos <nick.lontorfos@adidas.com>
2023-10-28 15:12:57 +0200

    basic server working

--- index.ts
+++ index.ts
@@ -1,1 +1,24 @@
-console.log("Hello via Bun!");
+import {type Serve} from 'bun'
+
+const server = Bun.serve({
+    // In development mode, Bun will surface errors in-browser with a built-in error page
+    // By default, development mode is enabled unless NODE_ENV is production
+    fetch(req: Request): Response | Promise<Response> {
+        const url = new URL(req.url);
+        if (url.pathname === "/") return new Response("Home page!");
+        if (url.pathname === "/blog") return new Response("Blog!");
+        
+        // return new Response("Hello World!");
+        throw new Error("woops!");
+      },
+    // Optional port number - the default value is 3000
+    port: process.env.PORT || 3000,
+    // Enable TLS by passing in a value for key and cert; both are required to enable TLS
+    // tls: {
+    //     key: Bun.file("./key.pem"),
+    //     cert: Bun.file("./cert.pem"),
+    // }
+    // hostname: "mydomain.com"
+}) satisfies Serve
+
+console.log(`Listening to PORT: ${server.port}`);
